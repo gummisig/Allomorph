bd31581a531476e68e34b1660e9158345441cfce
gummisig <gummisigurjons@gmail.com>
2014-05-15 16:03:59 +0000

    asd

---
+++ AllomorphSolution/Allomorph/Models/LinesAndTranslations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Allomorph.Models
+{
+
+    //Used for making a IEnumerable for the TextEdit view
+    public class LinesAndTranslations
+    {
+
+        public int SubFileId { get; set; }
+        public int SubLineId { get; set; }
+        public int LineNr { get; set; }
+        public string EngText { get; set; }
+        public string IceText { get; set; }
+    }
+}
