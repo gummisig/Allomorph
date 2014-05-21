4eda55254ffb43bdf48bb1396cd8ec2e1324cc15
BertelBB <bertel13@ru.is>
2014-05-21 00:46:02 +0000

    Like.cs og btn fixes

---
+++ AllomorphSolution/Allomorph/Models/Like.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Allomorph.Models
+{
+    public class Like
+    {
+        public int ID { get; set; }
+        [Required]
+        public int RequestID { get; set; }
+        [Required]
+        public string LikeUserName { get; set; }
+    }
+}
