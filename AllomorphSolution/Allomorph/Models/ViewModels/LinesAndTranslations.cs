00ff3b2aca6b4b5c3ef0bb7256eed9e96b256748
BertelBB <bertel13@ru.is>
2014-05-25 17:54:14 +0000

    ViewModels mappa

--- AllomorphSolution/Allomorph/Models/ViewModels/LinesAndTranslations.cs
+++ AllomorphSolution/Allomorph/Models/ViewModels/LinesAndTranslations.cs
@@ -8,10 +8,10 @@ namespace Allomorph.Models
     //Used for making a IEnumerable for the TextEdit view
     public class LinesAndTranslations
     {
+        public int FolderID { get; set; }
         public int SubFileID { get; set; }
         public int SubFileLineID { get; set; }
         public int LineNumber { get; set; }
-        public int FolderID { get; set; }
         public string EngText { get; set; }
         public string IceText { get; set; }
         public string SubFileLineStartTime { get; set; }
---
+++ AllomorphSolution/Allomorph/Models/ViewModels/TimeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Allomorph.Models
+{
+    public class TimeViewModel
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public int Milliseconds { get; set; }
+        public bool Negative { get; set; }
+
+        public TimeViewModel()
+        {
+            this.Hours = 0;
+            this.Minutes = 0;
+            this.Seconds = 0;
+            this.Milliseconds = 0;
+            this.Negative = false;
+        }
+    }
+}
